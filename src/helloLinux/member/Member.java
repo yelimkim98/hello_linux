f4b6c7f2ffaec4bf765cc8dca108ba6219d6b936
김예림 <kiel0103@naver.com>
2019-11-19 14:56:35 +0900

    create getter/setter in Member.java

--- src/helloLinux/member/Member.java
+++ src/helloLinux/member/Member.java
@@ -14,4 +14,71 @@ public class Member {
 	private String work;
 	private String belong;
 	private Date date;
+	
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public int getBirth_year() {
+		return birth_year;
+	}
+	public void setBirth_year(int birth_year) {
+		this.birth_year = birth_year;
+	}
+	public int getBirth_month() {
+		return birth_month;
+	}
+	public void setBirth_month(int birth_month) {
+		this.birth_month = birth_month;
+	}
+	public int getBirth_day() {
+		return birth_day;
+	}
+	public void setBirth_day(int birth_day) {
+		this.birth_day = birth_day;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getWork() {
+		return work;
+	}
+	public void setWork(String work) {
+		this.work = work;
+	}
+	public String getBelong() {
+		return belong;
+	}
+	public void setBelong(String belong) {
+		this.belong = belong;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
 }
